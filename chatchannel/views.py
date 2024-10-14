6f09f62a8cfd507e5d87be6b9fe6c020f88ef854
david30-maker <davidokpah396@gmail.com>
2024-10-14 21:08:06 +0100

    create url

--- chatchannel/views.py
+++ chatchannel/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 
-# Create your views here.
+@csrf_exempt
+def chat_view(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        message = data['message']
+        response = {
+            'message': message
+        }
+        return HttpResponse(json.dumps(response), content_type='application/json')
+    else:
+        return HttpResponse('Invalid request')
